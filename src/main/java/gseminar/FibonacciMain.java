2ecd85baeaaabfba32a3310793f3b713cabfc386
岡田滉太朗 <Okada@MacbookAiradmin-no-MacBook-Air.local>
2018-09-29 14:36:47 +0900

    implement done (うごかない)

---
+++ src/main/java/gseminar/FibonacciMain.java
@@ -0,0 +1,21 @@
+package gseminar;
+
+public class FibonacciMain {
+    public void run(String[] args){
+        if(args.length == 0){
+            throw new RequiredValueMissingException("no arguments were given");
+        }
+        perform(args);
+    }
+
+    private void perform(String[] args){
+        for (int i=0; i<args.length; i++) {
+            int value = Integer.valueOf(args[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        FibonacciMain main = new FibonacciMain();
+        main.run(args);
+    }
+}
